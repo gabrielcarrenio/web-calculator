1528f8c09ee177694739e0a0b6f1384d69cf62f4
Carlos Gabriel Carreño <carlosgabrielcarrenio@gmail.com>
2024-04-23 01:41:32 -0300

    working on the JavaScript logic

---
+++ logic.js
@@ -0,0 +1,12 @@
+function addNumber(num){
+    console.log(num);
+}
+
+
+function main() {
+    const idNumberTwo = document.getElementById("n2");
+    console.log(idNumberTwo);
+    addNumber(3);
+}
+
+main();
