3236dcd5125a4d47d60ee4e25b2056bc18bb4f29
Carlos Gabriel Carreño <carlosgabrielcarrenio@gmail.com>
2024-04-29 16:52:33 -0300

    work in the logic.js

--- logic.js
+++ logic.js
@@ -1,14 +1,25 @@
+let acum;
+
 function addNumber(num){
     console.log(num);
 }
 
 function handleClick(num) {
-    console.log(num, typeof num);
+    //console.log(num, typeof num);
+    if(typeof num == "number"){
+        showResult(acum)
+        let numStr = `${num}${acum}`
+        acum = parseInt(numStr); 
+    }
+}
+
+function showResult(data){
+    var result = document.querySelector(".result")
+    result.innerHTML = data
 }
 
 function main() {
-    var idNumberTwo = document.getElementById("n2");
-    
+    showResult("...");
 }
 
 main();
